36a907125679869b17df78767f47ac5d44c70edb
VESTnik <vasiliev.evgenij@gmail.com>
2023-06-02 22:05:32 +0200

    Vuetify style added. Status: working.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,24 @@
+// import {createApp} from 'vue'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(VueResource)
-
+Vue.use(Vuetify)
 new Vue({
     el: '#app',
     render: a => a(App)
 })
+
+
+
+// let app;
+// app = Vue.createApp({
+//     el: '#app',
+//     render: a => a(App),
+//     // vuetify: new Vuetify({})
+// });
+// app.use(VueResource)
+// app.use(Vuetify)
--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -1,29 +1,39 @@
 <template>
-    <div>
-    <div v-if="!profile">Необходимо авторизоваться через
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title>Sarafan</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span v-if="profile">{{profile.name}}</span>
+      <v-btn v-if="profile" icon href="/logout">
+        <v-icon>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container v-if="!profile">
+        Необходимо авторизоваться через
         <a href="/login">Google</a>
-    </div>
-    <div v-else>
-        <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
+      </v-container>
+      <v-container v-if="profile">
         <messages-list :messages="messages" />
-        </div>
-    </div>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    import MessagesList from 'components/messages/MessageList.vue'
+import MessagesList from 'components/messages/MessageList.vue'
 
-    export default {
-        components: {
-            MessagesList
-        },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
-            }
-        }
+export default {
+  components: {
+    MessagesList
+  },
+  data() {
+    return {
+      messages: frontendData.messages,
+      profile: frontendData.profile
     }
+  }
+}
 </script>
 
 <style>
